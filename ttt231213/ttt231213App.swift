b451f945301bf09638c3e5d73762cf4be45c6217
ttt231213 <judy881204@gmail.com>
2023-12-13 20:11:30 +0800

    Initial Commit

---
+++ ttt231213/ttt231213App.swift
@@ -0,0 +1,17 @@
+//
+//  ttt231213App.swift
+//  ttt231213
+//
+//  Created by 訪客使用者 on 2023/12/13.
+//
+
+import SwiftUI
+
+@main
+struct ttt231213App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
